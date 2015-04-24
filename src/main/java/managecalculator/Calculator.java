095bdeb4838570367d1514353cc1cb42d12ef09d
babela86 <lourencorafaela@gmail.com>
2015-04-24 15:22:28 +0100

    teste2

--- src/main/java/managecalculator/Calculator.java
+++ src/main/java/managecalculator/Calculator.java
@@ -4,6 +4,8 @@ import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import javax.faces.event.ActionEvent;
 
+import net.objecthunter.exp4j.*;
+
 import java.io.Serializable;
 
 @Named
@@ -11,9 +13,11 @@ import java.io.Serializable;
 public class Calculator implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private String expression;
+	private boolean clean;
 	
 	public Calculator() {
-		expression="";
+		expression = "0";
+		clean = true;
 	}
 
 	public String getExpression() {
@@ -24,18 +28,60 @@ public class Calculator implements Serializable {
 		expression = ex;
 	}
 
-	public void addToExpression(String add) {
-		expression+=add;
+	public void addToExpression(boolean c, String add) {
+		if (clean || c) expression=add;
+		else expression+=add;
 	}
 
-	public void clearExpression() {
-		expression="";
+	public String myResult() {
+		
+		double r = 0;
+		double e1 = Integer.parseInt(expression.charAt(0)+"");
+		char op = expression.charAt(1);
+		double e2 = Integer.parseInt(expression.charAt(2)+"");
+		
+		switch (op) {
+		case '+':
+			r = e1 + e2;
+			break;
+		case '-':
+			r = e1 - e2;
+			break;
+		case '*':
+			r = e1 * e2;
+			break;
+		case '/':
+			r = e1 / e2;
+		}
+		
+		if (r % 1 == 0) {
+			String s=r+"";
+			return s.substring(0, s.length()-2);
+		}
+		
+		return r+"";
+	}
+	
+	public String result() {
+		
+		Expression e = new ExpressionBuilder(expression).build();
+		double r = e.evaluate();
+		
+		if (r % 1 == 0) {
+			String s=r+"";
+			return s.substring(0, s.length()-2);
+		}
+		
+		/* qualquer coisa*/
+		return r+"";
+		
 	}
 
 	public void key(ActionEvent event) {
 		String add ="";
 		
-		boolean reset=false;
+		boolean c = false, r = false;
+		
 		switch (event.getComponent().getId()) {
 		case "number0": add = "0"; break;
 		case "number1": add = "1"; break;
@@ -51,22 +97,14 @@ public class Calculator implements Serializable {
 		case "minus": add = "-"; break;
 		case "times": add = "*"; break;
 		case "divide": add = "/"; break;
-		case "reset": reset=true;
+		case "result": add = result(); r = true; c = true; break;
+		case "reset": add = "0"; c = true; 
 		}
 		
-		if (reset) clearExpression();
-		else addToExpression(add);
+		addToExpression(c,add);
+		if (r || expression.equals("0")) clean = true;
+		else clean = false;
 				
-	}
-	
-	public Integer element(Integer i) {
-		if (i < expression.length()) //cuidado!!!
-			return Integer.parseInt(expression.substring(i,i+1));
-		else return 0;
-	}
-	
-	public Integer sumElement(Integer a, Integer b) {
-		return a+b;
 	}
 	
 }
