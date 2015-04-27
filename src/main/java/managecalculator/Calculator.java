95d295ec48866c0e3d4327f86f2e7f1be6081591
renatadiasilva <renatadiasilva@gmail.com>
2015-04-27 17:56:06 +0100

    calculadora básica

--- src/main/java/managecalculator/Calculator.java
+++ src/main/java/managecalculator/Calculator.java
@@ -28,9 +28,25 @@ public class Calculator implements Serializable {
 		expression = ex;
 	}
 
-	public void addToExpression(boolean c, String add) {
-		if (clean || c) expression=add;
-		else expression+=add;
+	public void addToExpression(int code, String add) {
+		if (clean && code != 4) expression=add;
+		else {
+			switch (code) {
+			case 0: expression += add; break;
+			case 1: expression = add; break;
+			case 2: if (expression.length() < 2) expression ="0";
+					else if (0 <= expression.length()-1) 
+						expression = expression.substring(0, expression.length()-1); break;
+			case 3: if (1 <= expression.length()) {
+						char first = expression.charAt(0);
+						if (first == '0') expression="0"; 
+						else if (first == '-') expression = expression.substring(1, expression.length());
+						else expression = "-"+expression; 
+					}
+					break;
+			case 4: expression = add; break;
+			}
+		}
 	}
 
 	public String myResult() {
@@ -62,25 +78,44 @@ public class Calculator implements Serializable {
 		return r+"";
 	}
 	
-	public String result() {
+	public String result(int code, String func) {
 		
-		Expression e = new ExpressionBuilder(expression).build();
-		double r = e.evaluate();
+		String ex = expression, s = "";
+		Expression e = new ExpressionBuilder(ex).build();
+
+		if (code != 0) ex = func+"("+expression+")";
+			
+		e = new ExpressionBuilder(ex).build();
 		
-		if (r % 1 == 0) {
-			String s=r+"";
-			return s.substring(0, s.length()-2);
-		}
+		try{
+			double r = e.evaluate();
 		
-		/* qualquer coisa*/
-		return r+"";
+			s = r+"";
+			if (r % 1 == 0) s = s.substring(0, s.length()-2);
+
+	    } catch(ArithmeticException ae){
+	    	s = "Erro: Divisão por zero!";
+	    }
 		
+		if (s.equals("NaN")) s = "Erro: Raiz de número negativo!";  //carregar a seguir ao erro (sqrt neg. e dp 1/x)
+		
+		// mais catchs
+		
+		// tem de se carregar duas vezes no 1/x??
+		
+		return s;
+				
 	}
+	
+	//	request (expressions) - aceder aos outros usar o inject
+	//	application (statistics)
+	//	session (historic)
 
 	public void key(ActionEvent event) {
 		String add ="";
 		
-		boolean c = false, r = false;
+		int code = 0;
+		boolean r = false;
 		
 		switch (event.getComponent().getId()) {
 		case "number0": add = "0"; break;
@@ -93,18 +128,29 @@ public class Calculator implements Serializable {
 		case "number7": add = "7"; break;
 		case "number8": add = "8"; break;
 		case "number9": add = "9"; break;
+		case "decPoint": add = "."; break;
+		case "plusMinus": code = 3; break;
 		case "plus": add = "+"; break;
 		case "minus": add = "-"; break;
 		case "times": add = "*"; break;
 		case "divide": add = "/"; break;
-		case "result": add = result(); r = true; c = true; break;
-		case "reset": add = "0"; c = true; 
+//		case "percent": add = "%"; break;
+		case "sqrt": add = result(4,"sqrt"); r = true; code = 4; break;
+		case "inverse": add = result(5,"1/"); r = true; code = 5; break;
+		case "deleteL": code = 2; break;
+		case "reset": add = "0"; code = 1; break;
+		case "result": add = result(0,""); r = true; code = 1; break;
 		}
 		
-		addToExpression(c,add);
+		// meter expressão em cima? tirar +/-
+		if (expression.equals("0") && code != 3) clean = true;
+
+		addToExpression(code,add);
+		
 		if (r || expression.equals("0")) clean = true;
 		else clean = false;
-				
+		
+		
 	}
 	
 }
